d10c22d9f4ae65bc93ed3d724db48a9a432e07ba
Restyled.io <commits@restyled.io>
2020-08-27 15:11:40 +0000

    Restyled by clang-format

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,37 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const rules = [
-    {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-            loader: 'babel-loader',
-        },
+  {
+    test : /\.(js|jsx)$/,
+    exclude : /node_modules/,
+    use : {
+      loader : 'babel-loader',
     },
-    {
-        test: /\.css$/,
-        use: [
-            {
-                loader: 'style-loader',
-            },
-            {
-                loader: 'css-loader',
-            },
-        ],
-    },
+  },
+  {
+    test : /\.css$/,
+    use : [
+      {
+        loader : 'style-loader',
+      },
+      {
+        loader : 'css-loader',
+      },
+    ],
+  },
 ];
 
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+  output : {
+    path : path.resolve(__dirname, 'build'),
+    filename : 'bundle.js',
+  },
+  resolve : {
+    modules : [ path.join(__dirname, 'src'), 'node_modules' ],
+    alias : {
+      react : path.join(__dirname, 'node_modules', 'react'),
     },
-    resolve: {
-        modules: [path.join(__dirname, 'src'), 'node_modules'],
-        alias: {
-            react: path.join(__dirname, 'node_modules', 'react'),
-        },
-    },
-    module: { rules },
-    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  },
+  module : {rules},
+  plugins : [ new HtmlWebpackPlugin({template : './src/index.html'}) ],
 };
