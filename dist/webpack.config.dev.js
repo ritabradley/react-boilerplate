76c696f50c72f4633ca02995ab7ea6eb2e1b203b
Rita Bradley <contact@ritabradley.com>
2020-08-26 20:31:34 -0700

    Fix eslint issue with linting dist folder

--- dist/webpack.config.dev.js
+++ dist/webpack.config.dev.js
@@ -1,24 +1,30 @@
-"use strict";
+'use strict'
 
-var path = require('path');
+var path = require('path')
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 
-var rules = [{
-  rules: [{
-    test: /\.s[ac]ss$/i,
+var rules = [
+  {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  {
+    test: /\.js$/,
     exclude: /node_modules/,
-    use: ['style-loader', 'css-loader', 'sass-loader']
-  }]
-}, {
-  test: /\.js$/,
-  exclude: /node_modules/,
-  use: ['babel-loader']
-}, {
-  test: /\.css$/,
-  exclude: /node_modules/,
-  use: ['style-loader', 'css-loader']
-}];
+    use: ['babel-loader']
+  },
+  {
+    test: /\.css$/,
+    exclude: /node_modules/,
+    use: ['style-loader', 'css-loader']
+  }
+]
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.js'),
   output: {
@@ -28,7 +34,9 @@ module.exports = {
   module: {
     rules: rules
   },
-  plugins: [new HtmlWebpackPlugin({
-    template: './public/index.html'
-  })]
-};
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
--- src/dist/index.dev.js
+++ src/dist/index.dev.js
@@ -1,10 +1,12 @@
-"use strict";
+'use strict'
 
-var _react = _interopRequireDefault(require("react"));
+var _react = _interopRequireDefault(require('react'))
 
-var _reactDom = require("react-dom");
+var _reactDom = require('react-dom')
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
 
-var root = document.getElementById('root');
-(0, _reactDom.render)(root);
+var root = document.getElementById('root')
+;(0, _reactDom.render)(root)
